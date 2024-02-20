5c341e14e50f0524b097e3d4af881b86c4cc7012
Marianne Pemmerl <91166970+pem-ms@users.noreply.github.com>
2024-02-19 20:50:05 -0500

    Update dobble_verificator.py

--- Devoir1/dobble_verificator.py
+++ Devoir1/dobble_verificator.py
@@ -1,77 +1,11 @@
-#Nom, Matricule
-#Nom, Matricule
+#Marianne Schmit Pemmerl, 20192143
+#Kanty-Louange Gakima, Matricule
 
 # cette classe sert a verifier la validite de l'ensemble des cartes du jeu dans le fichier cartes.txt
 # this class is used to check the validity  of the game cards set in the cartes.txt file
 
 # doit retourner 0 si tout est correct, 1 si le jeu n'est pas optimal selon l'ordre et 2 si le jeu n'est pas valide
 # should return 0 if everything is correct, 1 if the game set is not optimal according to the order and 2 if the game set is invalid
-
-#import os.path
-
-
-
-def verifyOptimalCardsno(cards, n):
-    excepted_cards_n = n**2+n+1
-    count_cards = len(cards)
-    return count_cards==excepted_cards_n
-
-
-def verifyOptimalSymbolsno(cards, n):
-    excepted_symbols_n = n**2+n+1
-    symbols = []
-    for card in cards:
-        for symbol in card:
-            if symbol not in symbols:
-                symbols.append(symbol)
-    return len(symbols)==excepted_symbols_n
-
-
-#verify if every card has same symbols number
-def verifySymbolsno(cards): 
-    first_card = cards[0]
-    for card in cards[1:]:
-        if len(first_card) != len(card):
-            return False
-    return True
-
-
-def hasOneCommonSymbol(card1, card2):
-    # Find the intersection of symbols between card1 and card2
-    common_symbols = set(card1) & set(card2)  
-    return len(common_symbols) == 1
-
-
-#verify if each pair of cards shares only one common symbol
-def verifySymbolsperPair(cards):
-    for card1 in range(len(cards)):
-        for card2 in range(len(cards)):
-            if card1 != card2:
-                if not hasOneCommonSymbol(cards[card1], cards[card2]):
-                    return False
-    return True
-
-
-#verify if each card has n+1 symbols 
-def hasNplusOneSymbols(cards, n): 
-
-    for card in cards:
-        if len(card) != n+1:
-            return False
-    return True
-
-
-def read_file(input_file):
-    file = open(input_file,"r")
-    lines = file.readlines()
-    file.close()
-    return lines
-
-
-
-##### or get it from input??????????????????????????????????????????????????????????????????????????????????????
-def findOrder(cards):
-    return len(cards[0])-1
 
 class Verificator():
     def __init__(self):
@@ -81,7 +15,7 @@ class Verificator():
         if verbose :
             print("***Verification des cartes***")
         
-        cards = read_file(cards_file)
+        cards = self.read_file(cards_file)
 
         symbols_split_cards = [] 
 
@@ -90,23 +24,22 @@ class Verificator():
 
         cards = symbols_split_cards
 
-        order = findOrder(cards) #####################################################################
+        order = self.findOrder(cards) #####################################################################
 
         # test : le nombre de carte devrait être optimal
-        cardsno_isOptimal = verifyOptimalCardsno(cards, order)
+        cardsno_isOptimal = self.verifyOptimalCardsno(cards, order)
       
         # test : le nombre de symboles par carte est le même pour chaque carte
-        has_same_no_symbols = verifySymbolsno(cards)
+        has_same_no_symbols =  self.verifySymbolsno(cards)
             
         # test : chaque paire de cartes partagent toujours un et un seul symbole en commun
-
-        has_one_Common_Symbol= verifySymbolsperPair(cards)
+        has_one_Common_Symbol= self.verifySymbolsperPair(cards)
             
         # test : le nombre de symbole total devrait être optimal
-        symbolsno_isOptimal= verifyOptimalSymbolsno(cards, order)
+        symbolsno_isOptimal= self.verifyOptimalSymbolsno(cards, order)
 
 
-        has_n_plus_one_Symbols= hasNplusOneSymbols(cards, order)
+        has_n_plus_one_Symbols= self.hasNplusOneSymbols(cards, order)
 
 
         # succes (0) si le jeu est valide et optimal
@@ -120,3 +53,62 @@ class Verificator():
         else:
             return 2
 
+
+
+    def verifyOptimalCardsno(self, cards, n):
+        excepted_cards_n = n**2+n+1
+        count_cards = len(cards)
+        return count_cards==excepted_cards_n
+
+
+    def verifyOptimalSymbolsno(self, cards, n):
+        excepted_symbols_n = n**2+n+1
+        symbols = []
+        for card in cards:
+            for symbol in card:
+                if symbol not in symbols:
+                    symbols.append(symbol)
+        return len(symbols)==excepted_symbols_n
+    
+
+    #verify if every card has same symbols number
+    def verifySymbolsno(self, cards): 
+        first_card = cards[0]
+        for card in cards[1:]:
+            if len(first_card) != len(card):
+                return False
+        return True
+
+
+    def hasOneCommonSymbol(self, card1, card2):
+        # Find the intersection of symbols between card1 and card2
+        common_symbols = set(card1) & set(card2)  
+        return len(common_symbols) == 1
+
+    #verify if each pair of cards shares only one common symbol
+    def verifySymbolsperPair(self, cards):
+        for card1 in range(len(cards)):
+            for card2 in range(len(cards)):
+                if card1 != card2:
+                    if not self.hasOneCommonSymbol(cards[card1], cards[card2]):
+                        return False
+        return True
+        
+    #verify if each card has n+1 symbols 
+    def hasNplusOneSymbols(self, cards, n): 
+
+        for card in cards:
+            if len(card) != n+1:
+                return False
+        return  True
+
+
+    def read_file(self, input_file):
+        file = open(input_file,"r")
+        lines = file.readlines()
+        file.close()
+        return lines
+
+    #calculate order
+    def findOrder(self, cards):
+        return len(cards[0])-1
